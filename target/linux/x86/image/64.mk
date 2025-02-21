5b49a09443c8e32426bfbdc1cabac771386db9f3
yujieli <yujieli@163.com>
2025-02-21 19:20:21 +0800

    恢复缺省的kmod核心驱动，增加kmod-macvlan

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -2,8 +2,9 @@ define Device/generic
   DEVICE_VENDOR := Generic
   DEVICE_MODEL := x86/64
   DEVICE_PACKAGES += \
-	kmod-e1000e \
-	kmod-igb \
+  kmod-amazon-ena kmod-amd-xgbe kmod-bnx2 \
+	kmod-e1000e kmod-e1000 kmod-forcedeth kmod-fs-vfat \
+	kmod-igb kmod-igc kmod-ixgbe kmod-r8168 kmod-tg3 \
 	kmod-macvlan
   GRUB2_VARIANT := generic
 endef
