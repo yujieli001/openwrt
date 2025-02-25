9a8bbdadc13618583fb0edeecddad68ef3d6cd39
yujieli <yujieli@163.com>
2025-02-25 21:56:32 +0800

    去除docker, luci-app-dockerman

--- include/target.mk
+++ include/target.mk
@@ -71,8 +71,8 @@ DEFAULT_PACKAGES.router:=\
 	default-settings \
   luci-proto-wireguard \
 	ddns-scripts_aliyun \
-  dockerd \
-  luci-app-dockerman \
+  #dockerd \
+  #luci-app-dockerman \
   luci-app-ddns \
 	luci-app-wol \
 	luci-app-wrtbwmon \
